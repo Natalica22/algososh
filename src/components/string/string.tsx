11f6a637cc898051dfbe88291f2ffa8dd5202744
Natalia Doroshenko <Natalica.22@yandex.ru>
2024-01-07 14:43:47 +0700

    feat: разметка строка

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import styles from "./string.module.css";
+import { Circle } from "../ui/circle/circle";
 
 export const StringComponent: React.FC = () => {
   return (
     <SolutionLayout title="Строка">
-     
+      <div className={styles.container}>
+        <form className={styles.form}>
+          <Input maxLength={11} isLimitText={true} />
+          <Button text='Развернуть' disabled={false} isLoader={false} />
+        </form>
+        <div className={styles.circles}>
+          {['1', '2', '3'].map(elem => <Circle letter={elem} />)}
+        </div>
+      </div>
     </SolutionLayout>
   );
 };
