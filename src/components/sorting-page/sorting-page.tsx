31a6cbdcdb8af5addb89557b9b95e61e14c5d168
Natalia Doroshenko <Natalica.22@yandex.ru>
2024-01-08 23:35:15 +0700

    feat: реализация методов сортировки

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -6,6 +6,12 @@ import { Button } from "../ui/button/button";
 import { Direction } from "../../types/direction";
 import { Column } from "../ui/column/column";
 import { ElementStates } from "../../types/element-states";
+import { delay } from "../../utils/delay";
+
+enum SortingMethod {
+  Selection = "selection",
+  Bubble = "bubble"
+}
 
 type Value = {
   value: number;
@@ -15,12 +21,22 @@ type Value = {
 const MAX_ARRAY_SIZE = 17;
 const MIN_ARRAY_SIZE = 3;
 
+const swap = (arr: Value[], firstIndex: number, secondIndex: number): void => {
+  const temp = arr[firstIndex];
+  arr[firstIndex] = arr[secondIndex];
+  arr[secondIndex] = temp;
+};
+
 const getRandomFromInterval = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 export const SortingPage: React.FC = () => {
   const [array, setArray] = useState([] as Value[]);
+  const [method, setMethod] = useState(SortingMethod.Selection);
+
+  const [ascendingInProgress, setAscendingInProgress] = useState(false);
+  const [descendingInProgress, setDescendingInProgress] = useState(false);
 
   const onGenerateArrayButtonClick = (event: React.MouseEvent<HTMLElement>) => {
     randomArr();
@@ -39,19 +55,83 @@ export const SortingPage: React.FC = () => {
     setArray([...arr]);
   }
 
+  const onChangeMethod = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+    setMethod(value as SortingMethod);
+  }
+
+  const onAscendingClick = async (event: React.MouseEvent<HTMLElement>) => {
+    setAscendingInProgress(true);
+
+    await sortArray(false);
+
+    setAscendingInProgress(false);
+  }
+
+  const onDescendinClick = async (event: React.MouseEvent<HTMLElement>) => {
+    setDescendingInProgress(true);
+
+    await sortArray(true);
+
+    setDescendingInProgress(false);
+  }
+
+  const sortArray = async (desc: boolean) => {
+    if (method === SortingMethod.Selection) {
+      await selectionSort(desc);
+    } else {
+      await bubbleSort(desc);
+    }
+  }
+
+  const selectionSort = async (desc: boolean) => {
+    const arr = [...array];
+    const { length } = arr;
+
+    for (let i = 0; i < length - 1; i++) {
+      let maxInd = i;
+      for (let j = i + 1; j < length; j++) {
+        if ((desc && arr[j].value > arr[maxInd].value) || (!desc && arr[j].value < arr[maxInd].value)) {
+          maxInd = j;
+        }
+      }
+      if (maxInd !== i) {
+        swap(arr, i, maxInd)
+      }
+    }
+
+    setArray([...arr]);
+  }
+
+  const bubbleSort = async (desc: boolean) => {
+    const arr = [...array];
+
+    for (let j = arr.length - 1; j > 0; j--) {
+      for (let i = 0; i < j; i++) {
+        if ((desc && arr[i].value < arr[i + 1].value) || (!desc && arr[i].value > arr[i + 1].value)) {
+          swap(arr, i, i + 1);
+        }
+      }
+    }
+
+    setArray([...arr]);
+  }
+
   return (
     <SolutionLayout title="Сортировка массива">
       <div className={styles.container} >
         <div className={styles.menu}>
           <form className={styles.method}>
-            <RadioInput label='Выбор' name='method' />
-            <RadioInput label='Пузырёк' name='method' />
+            <RadioInput label='Выбор' name='method' value={SortingMethod.Selection} onChange={onChangeMethod} checked={method === SortingMethod.Selection} />
+            <RadioInput label='Пузырёк' name='method' value={SortingMethod.Bubble} onChange={onChangeMethod} checked={method === SortingMethod.Bubble} />
           </form>
           <div className={styles.direction}>
-            <Button text='По возрастанию' sorting={Direction.Ascending} extraClass={styles.button} />
-            <Button text='По убыванию' sorting={Direction.Descending} extraClass={styles.button} />
+            <Button text='По возрастанию' sorting={Direction.Ascending} extraClass={styles.button} onClick={onAscendingClick}
+              isLoader={ascendingInProgress} disabled={descendingInProgress || array.length === 0} />
+            <Button text='По убыванию' sorting={Direction.Descending} extraClass={styles.button} onClick={onDescendinClick}
+              isLoader={descendingInProgress} disabled={ascendingInProgress || array.length === 0} />
           </div>
-          <Button text='Новый массив' extraClass={styles.button} onClick={onGenerateArrayButtonClick} />
+          <Button text='Новый массив' extraClass={styles.button} onClick={onGenerateArrayButtonClick} disabled={ascendingInProgress || descendingInProgress} />
         </div>
         <div className={styles.columns}>
           {array.map((elem, i) => <Column index={elem.value} state={elem.state} key={i} />)}
