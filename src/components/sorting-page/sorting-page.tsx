481885778632d628fe5b8c670a95898c8bdd57f6
Natalia Doroshenko <Natalica.22@yandex.ru>
2024-01-08 22:37:09 +0700

    feat: создание массива

--- src/components/sorting-page/sorting-page.tsx
+++ src/components/sorting-page/sorting-page.tsx
@@ -1,12 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
 import styles from "./sorting-page.module.css";
 import { RadioInput } from "../ui/radio-input/radio-input";
 import { Button } from "../ui/button/button";
 import { Direction } from "../../types/direction";
 import { Column } from "../ui/column/column";
+import { ElementStates } from "../../types/element-states";
+
+type Value = {
+  value: number;
+  state: ElementStates;
+}
+
+const MAX_ARRAY_SIZE = 17;
+const MIN_ARRAY_SIZE = 3;
+
+const getRandomFromInterval = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 export const SortingPage: React.FC = () => {
+  const [array, setArray] = useState([] as Value[]);
+
+  const onGenerateArrayButtonClick = (event: React.MouseEvent<HTMLElement>) => {
+    randomArr();
+  }
+
+  const randomArr = () => {
+    const size = getRandomFromInterval(MIN_ARRAY_SIZE, MAX_ARRAY_SIZE);
+    const arr = new Array<Value>(size);
+
+    for (let i = 0; i < size; i++) {
+      arr[i] = {
+        value: getRandomFromInterval(0, 100),
+        state: ElementStates.Default
+      }
+    }
+    setArray([...arr]);
+  }
+
   return (
     <SolutionLayout title="Сортировка массива">
       <div className={styles.container} >
@@ -19,10 +51,10 @@ export const SortingPage: React.FC = () => {
             <Button text='По возрастанию' sorting={Direction.Ascending} extraClass={styles.button} />
             <Button text='По убыванию' sorting={Direction.Descending} extraClass={styles.button} />
           </div>
-          <Button text='Новый массив' extraClass={styles.button} />
+          <Button text='Новый массив' extraClass={styles.button} onClick={onGenerateArrayButtonClick} />
         </div>
         <div className={styles.columns}>
-          {[1, 50, 0, 10, 45, 100].map((elem, i) => <Column index={elem} key={i} />)}
+          {array.map((elem, i) => <Column index={elem.value} state={elem.state} key={i} />)}
         </div>
       </div>
     </SolutionLayout>
