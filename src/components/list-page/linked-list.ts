88b0527afe9d77e4d21b0eaf28696481dbfcb3e3
Natalia Doroshenko <Natalica.22@yandex.ru>
2024-01-14 22:35:56 +0700

    feat: класс LinkedList

---
+++ src/components/list-page/linked-list.ts
@@ -0,0 +1,135 @@
+class Node<T> {
+  constructor(public value: T, public next: Node<T> | null) { }
+}
+
+export class LinkedList<T> {
+  private head: Node<T> | null = null;
+  private tail: Node<T> | null = null;
+  private size: number = 0;
+
+  prepend(item: T) {
+    const node = new Node(item, this.head);
+    this.head = node;
+    if (this.tail === null) {
+      this.tail = node;
+    }
+    this.size += 1;
+  }
+
+  append(item: T) {
+    const node = new Node(item, null);
+    if (this.tail !== null) {
+      this.tail.next = node;
+    }
+    this.tail = node;
+    if (this.head === null) {
+      this.head = node;
+    }
+    this.size += 1;
+  }
+
+  removeHead() {
+    if (this.head === null) {
+      throw Error('List is empty');
+    }
+    this.head = this.head.next;
+    this.size -= 1;
+    if (this.size === 0) {
+      this.tail = null;
+    }
+  }
+
+  removeTail() {
+    if (this.head === null) {
+      throw Error('List is empty');
+    }
+    let node: Node<T> | null = this.head;
+    while (node !== null) {
+      if (node.next === this.tail) {
+        this.tail = node;
+        break;
+      }
+      node = node.next;
+    }
+    this.size -= 1;
+    if (this.size === 0) {
+      this.tail = null;
+    }
+  }
+
+  insertAt(item: T, index: number) {
+    if (index < 0 || index >= this.size) {
+      throw Error('Index is out of bounds');
+    }
+    if (this.head === null) {
+      throw Error('List is empty');
+    }
+
+    if (index === 0) {
+      this.prepend(item);
+      return;
+    }
+
+    let preNode: Node<T> = this.head;
+    let node: Node<T> | null = this.head?.next || null;
+    let count = 1;
+    while (node !== null) {
+      if (count === index) {
+        const newNode = new Node(item, node);
+        preNode.next = newNode;
+        break;
+      }
+      preNode = node;
+      node = node.next;
+      count += 1;
+    }
+    this.size += 1;
+  }
+
+  removeAt(index: number) {
+    if (index < 0 || index >= this.size) {
+      throw Error('Index is out of bounds');
+    }
+    if (this.head === null) {
+      throw Error('List is empty');
+    }
+
+    if (index === 0) {
+      this.removeHead();
+      return;
+    }
+
+    let preNode: Node<T> = this.head;
+    let node: Node<T> | null = this.head?.next || null;
+    let count = 1;
+    while (node !== null) {
+      if (count === index) {
+        preNode.next = node.next;
+        if (preNode.next === null) {
+          this.tail = preNode;
+        }
+        break;
+      }
+      preNode = node;
+      node = node.next;
+      count += 1;
+    }
+    this.size -= 1;
+  }
+
+  toArray() {
+    const arr = [];
+    let node: Node<T> | null = this.head?.next || null;
+    let count = 0;
+    while (node !== null) {
+      arr[count] = node.value;
+      node = node.next;
+      count += 1;
+    }
+    return arr;
+  }
+
+  getSize() {
+    return this.size;
+  }
+}
